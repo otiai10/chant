c5fe9192f84b05dd9ad4575d862c2e47b01c26ef
otiai10 <otiai10@gmail.com>
2015-07-05 00:14:38 +0900

    Fix #92

--- client/dest/_chant.desktop.js
+++ client/dest/_chant.desktop.js
@@ -92,6 +92,64 @@ chant.clearUnread = function(ev) {
 };
 
 
+var AnchorizableText = React.createClass({displayName: "AnchorizableText",
+    // render it first
+    render: function () {
+        return React.createElement('p', {
+            className: 'line-wrap',
+            ref: 'ATSelf'
+        }, this.props.text);
+    },
+    // anchorize it after mount
+    componentDidMount: function() {
+        this.anchorize();
+    },
+    // anchorize it after update
+    componentDidUpdate: function() {
+        this.anchorize();
+    },
+    // anchorize execution
+    anchorize: function() {
+        var myself = React.findDOMNode(this.refs.ATSelf);
+        var value = this.props.text;
+        var self = this;
+        for (var i = 0; i < this.props.ExprWrappers.length; i++) {
+          var v = this.exprAndWrap(value, this.props.ExprWrappers[i]);
+          if (v) {
+            myself.innerHTML = v;
+            return;
+          }
+        }
+        myself.innerHTML = value;
+    },
+    // expr and wrap
+    exprAndWrap: function(value, ew /* interface ExprWrapper */) {
+        if (typeof ew.expr != 'function' || typeof ew.wrap != 'function') return value;
+        var matches = ew.expr().exec(value) || [];
+        if (matches.length === 0) return null;
+        value = value.split(matches[0]).join(ew.wrap(matches[0]));
+        return value;
+    },
+    getDefaultProps: function() {
+        var sampleExprWrapper = {
+            expr: function () /* RegExp */ {
+                return /((https?):\/\/|www\.)([a-z0-9-]+\.)+[a-z0-9:]+(\/[^\s<>"',;]*)?(jpe?g|png|gif)/gi;
+            },
+            wrap: function (value) /* string */ {
+                return '<a href="' + value + '" target="_blank"><img class="entry-image" src="' + value + '" /></a>';
+            }
+        };
+        var normalURLExprWrapper = {
+            expr: function() { return /(https?):\/\/([_a-zA-Z0-9-.@&=!~*()\';/?:+$,%#]+)/gi; },
+            wrap: function(value) { return '<a href="' + value + '" target="_blank">' + value + '</a>'; }
+        };
+        return {
+            ExprWrappers: [sampleExprWrapper, normalURLExprWrapper]
+        };
+    }
+});
+
+
 var Stamps = React.createClass({displayName: "Stamps",
   render: function() {
     var stamps = this.props.stamps.map(function(stamp) {
@@ -121,56 +179,6 @@ var Stamp = React.createClass({displayName: "Stamp",
   useStamp: function () {
     chant.Send("stampuse", this.props.stamp.source.raw);
   }
-});
-
-
-var AnchorizableText = React.createClass({displayName: "AnchorizableText",
-    // render it first
-    render: function () {
-        return React.createElement('p', {
-            className: 'line-wrap',
-            ref: 'ATSelf'
-        }, this.props.text);
-    },
-    // anchorize it after mount
-    componentDidMount: function() {
-        this.anchorize();
-    },
-    // anchorize it after update
-    componentDidUpdate: function() {
-        this.anchorize();
-    },
-    // anchorize execution
-    anchorize: function() {
-        var myself = React.findDOMNode(this.refs.ATSelf);
-        var value = this.props.text;
-        var self = this;
-        this.props.ExprWrappers.map(function(ew) {
-          value = self.exprAndWrap(value, ew);
-        });
-        myself.innerHTML = value;
-    },
-    // expr and wrap
-    exprAndWrap: function(value, ew /* interface ExprWrapper */) {
-        if (typeof ew.expr != 'function' || typeof ew.wrap != 'function') return value;
-        var matches = ew.expr().exec(value) || [];
-        if (matches.length === 0) return value;
-        value = value.split(matches[0]).join(ew.wrap(matches[0]));
-        return value;
-    },
-    getDefaultProps: function() {
-        var sampleExprWrapper = {
-            expr: function () /* RegExp */ {
-                return /((https?):\/\/|www\.)([a-z0-9-]+\.)+[a-z0-9:]+(\/[^\s<>"',;]*)?(jpe?g|png|gif)/gi;
-            },
-            wrap: function (value) /* string */ {
-                return '<a href="' + value + '" target="_blank"><img class="entry-image" src="' + value + '" /></a>';
-            }
-        };
-        return {
-            ExprWrappers: [sampleExprWrapper]
-        };
-    }
 });
 
 /**
--- client/src/common/views/anchorizable.jsx
+++ client/src/common/views/anchorizable.jsx
@@ -20,30 +20,38 @@ var AnchorizableText = React.createClass({
         var myself = React.findDOMNode(this.refs.ATSelf);
         var value = this.props.text;
         var self = this;
-        this.props.ExprWrappers.map(function(ew) {
-          value = self.exprAndWrap(value, ew);
-        });
+        for (var i = 0; i < this.props.ExprWrappers.length; i++) {
+          var v = this.exprAndWrap(value, this.props.ExprWrappers[i]);
+          if (v) {
+            myself.innerHTML = v;
+            return;
+          }
+        }
         myself.innerHTML = value;
     },
     // expr and wrap
     exprAndWrap: function(value, ew /* interface ExprWrapper */) {
         if (typeof ew.expr != 'function' || typeof ew.wrap != 'function') return value;
         var matches = ew.expr().exec(value) || [];
-        if (matches.length === 0) return value;
+        if (matches.length === 0) return null;
         value = value.split(matches[0]).join(ew.wrap(matches[0]));
         return value;
     },
     getDefaultProps: function() {
         var sampleExprWrapper = {
             expr: function () /* RegExp */ {
-                return /((https?):\/\/|www\.)([a-z0-9-]+\.)+[a-z0-9:]+(\/[^\s<>"',;]*)?(jpe?g|png|gif)$/gi;
+                return /((https?):\/\/|www\.)([a-z0-9-]+\.)+[a-z0-9:]+(\/[^\s<>"',;]*)?(jpe?g|png|gif)/gi;
             },
             wrap: function (value) /* string */ {
-                return '<a href="' + value + '"><img class="entry-image" src="' + value + '" /></a>';
+                return '<a href="' + value + '" target="_blank"><img class="entry-image" src="' + value + '" /></a>';
             }
         };
+        var normalURLExprWrapper = {
+            expr: function() { return /(https?):\/\/([_a-zA-Z0-9-.@&=!~*()\';/?:+$,%#]+)/gi; },
+            wrap: function(value) { return '<a href="' + value + '" target="_blank">' + value + '</a>'; }
+        };
         return {
-            ExprWrappers: [sampleExprWrapper]
+            ExprWrappers: [sampleExprWrapper, normalURLExprWrapper]
         };
     }
 });
