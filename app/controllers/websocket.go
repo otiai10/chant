8ed5394ed1337e532bff9f4248bd37458ecffebf
otiai10 <otiai10@gmail.com>
2016-01-13 15:17:47 +0900

    Close no longer used websocket.Connection IO

--- app/controllers/websocket.go
+++ app/controllers/websocket.go
@@ -17,6 +17,7 @@ type ChantSocket struct {
 // websocketが繋がったときの挙動を定義している
 func (c ChantSocket) RoomSocket(ws *websocket.Conn, id, token string) revel.Result {
 
+	// セッション無きものは去れ
 	user, err := models.RestoreUserFromJSON(c.Session["user_raw"])
 	if err != nil {
 		revel.ERROR.Println(err)
@@ -25,36 +26,45 @@ func (c ChantSocket) RoomSocket(ws *websocket.Conn, id, token string) revel.Resu
 
 	myroom := chatroom.GetRoom("default", token)
 
-	// 自分のJoinを自分に伝えるために、Subscribeのあとに呼ぶ
+	// 自分の参加をroomに表明し、新着イベントの購読チャンネルを委譲してもらう
 	subscription := myroom.Subscribe(user)
-	// 自分自身のjoinイベントを確実に自分自身へ送る
+
+	// 自分自身のJoinを確実に自分に伝えるために、Subscribeのあとに独自で呼ぶ
 	websocket.JSON.Send(ws, myroom.Join(user))
+
 	defer func() {
 		// 自分のLeaveを自分に送ってロックしてしまうので、drainをさきに呼ぶ
 		myroom.Unsubscribe(subscription)
 		myroom.Leave(user)
+
+		// いらなくなったsocket connectionのIOはちゃんとcloseする
+		ws.Close()
 	}()
 
 	// 自分自身のソケットから来る発言を受け取るチャンネル
 	myself := make(chan string)
+
 	// 自分自身のソケットから来る発言を流すgoroutineを流す
 	go listenMyself(ws, myself)
 
 	// トゥッティ!　私たちが心を奪う!!
 	for {
 		select {
+
+		// だれかからのイベント受け取りの処理
+		// chatroomへのsubscriptionから新しいイベントが出てきたら
+		// それを自分のwebsocketに流し込む
 		case event := <-subscription.New:
-			// chatroomへのsubscriptionから新しいイベントが出てきたら
-			// それを自分のwebsocketに流し込む
 			if websocket.JSON.Send(ws, &event) != nil {
-				revel.INFO.Println("websocket.JSON.Sendに失敗したら、このループを終了してdeferを呼ぶ")
-				return nil
+				return nil // websocket.JSON.Sendに失敗したら、このループを終了してdeferを呼ぶ
 			}
+
+		// 自分からのイベントの発信処理
+		// 自分自身のsocketをlistenしているチャンネルからなにかしらのメッセージが出てきたら
+		// roomへそれを伝える
 		case msg, ok := <-myself:
-			// 自分のソケットからの送信をproxyするチャンネルを受ける.
 			if !ok {
-				revel.INFO.Println("myselfチャンネルがcloseしていれば、このループを終了してdeferを呼ぶ")
-				return nil
+				return nil // myselfチャンネルがcloseしていれば、このループを終了してdeferを呼ぶ
 			}
 			myroom.Say(user, msg)
 		}
