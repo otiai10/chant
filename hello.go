77e6f381bb0675d29fbe1591d53dec6a6f767e74
Hiromu OCHIAI <otiai10@gmail.com>
2017-04-21 08:18:16 +0900

    calm down, clean up

--- hello.go
+++ hello.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/otiai10/marmoset"
 )
 
-// func main() {
-// 	log.Print("Listening on port 8080")
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
-
-func init() {
-	router := marmoset.NewRouter()
-	router.GET("/", handler)
-	// http.HandleFunc("/", handler)
+func main() {
+	http.HandleFunc("/", handle)
 	http.HandleFunc("/_ah/health", healthCheckHandler)
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, marmoset")
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello world!")
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
