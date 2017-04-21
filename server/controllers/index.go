4529910d88ea0b8427463bd9d63d3183ed164801
Hiromu OCHIAI <otiai10@gmail.com>
2017-04-21 11:43:20 +0900

    [test] Pass Env Vars with includes directive

--- server/controllers/index.go
+++ server/controllers/index.go
@@ -3,13 +3,16 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/otiai10/marmoset"
 )
 
 // Index ...
 func Index(w http.ResponseWriter, r *http.Request) {
-	marmoset.Render(w).HTML("index", nil)
+	marmoset.Render(w).HTML("index", marmoset.P{
+		"test": os.Getenv("MY_VAR"),
+	})
 }
 
 // HealthCheck ...
