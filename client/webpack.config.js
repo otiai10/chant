cf0a7deafa9e04f9e3872ef7fed03d404d571d9c
Hiromu OCHIAI <otiai10@gmail.com>
2017-05-25 21:13:00 +0900

    Set up webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Client build command should be executed on the project top,
+ * where "package.json" located.
+ */
+
+var webpack = require("webpack");
+
+var plugins = [
+  new webpack.DefinePlugin({
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
+  }),
+];
+if (process.env.NODE_ENV == "production") {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    // compress: { warnings: false },
+    mangle: true
+  }));
+}
+
+module.exports = {
+  entry: {
+    index: "./client/src/entrypoints/index.js"
+  },
+  output: {
+    filename: "./app/public/js/[name].js"
+  },
+  plugins: plugins,
+};
