5563c3ebfdcc4194afb9a34efebe8d643bf1e51f
Hiromu OCHIAI <otiai10@gmail.com>
2017-06-13 17:58:57 +0900

    Show only connected members

--- client/src/js/Components/Members/index.js
+++ client/src/js/Components/Members/index.js
@@ -7,10 +7,15 @@ import Member from './Member';
 export default class Members extends Component {
   render() {
     const {members} = this.props;
+    const current = Object.keys(members).filter(id => {
+      if (id == chant.user.id) return false;
+      if (!members[id].browsers) return false;
+      return true;
+    });
     return (
       <div id="members" className="row section">
         <Myself user={chant.user} />
-        {Object.keys(members).filter(id => id != chant.user.id).map(id => <Member key={id} member={members[id]} />)}
+        {current.map(id => <Member key={id} member={members[id]} />)}
       </div>
     );
   }
