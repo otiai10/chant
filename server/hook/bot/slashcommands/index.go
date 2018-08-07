8b9e7943046985a12de639ef3a3904ec3603c412
Hiromu OCHIAI <otiai10@gmail.com>
2018-08-07 18:11:27 +0900

    Add command to show upcoming typhoon information

--- server/hook/bot/slashcommands/index.go
+++ server/hook/bot/slashcommands/index.go
@@ -2,14 +2,15 @@ package slashcommands
 
 // Commands registry
 var Commands = map[string]SlashCommand{
-	"/amesh":  Amesh{},
-	"/img":    new(Img),
-	"/gif":    Gif{},
-	"/ggl":    Google{},
-	"/hello":  Hello{},
-	"/help":   Help{},
-	"/tz":     Timezone{},
-	"/pins":   Pins{},
-	"/pin":    Pins{},
-	"/reload": Reload{},
+	"/amesh":   Amesh{},
+	"/img":     new(Img),
+	"/gif":     Gif{},
+	"/ggl":     Google{},
+	"/hello":   Hello{},
+	"/help":    Help{},
+	"/tz":      Timezone{},
+	"/pins":    Pins{},
+	"/pin":     Pins{},
+	"/reload":  Reload{},
+	"/typhoon": Typhoon{},
 }
